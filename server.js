2344efb9e300cdadae17a40193323a77711061d2
combikms <combikms@naver.com>
2025-04-13 23:24:07 +0900

    :sparkles: Feat: MongoDB 연동

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
 const app = express()
+const url = process.env.MONGODB_URL
+let db
+
+new MongoClient(url).connect().then((client) => {
+    console.log('DB 연결 성공')
+    db = client.db('StrengthHub')
+}).catch((err) => {
+    console.log(err)
+})
 
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
